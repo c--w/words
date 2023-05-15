a743867b66214d4d60d267265345513555bcd5f8
c--w <zevrnc@gmail.com>
2023-05-15 13:58:11 +0200

    fix sw

--- sw.js
+++ sw.js
@@ -1,45 +1,57 @@
 
 // Change this to your repository name
 var GHPATH = '/words';
- 
+
 // Choose a different app prefix name
 var APP_PREFIX = 'words_';
- 
+
 // The version of the cache. Every time you change any of the files
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
 var VERSION = 'version_28';
- 
+
 // The files to make available for offline use. make sure to add 
 // others to this list
-var URLS = [    
-  `${GHPATH}/`,
-  `${GHPATH}/fonts/bootstrap-icons.woff`,
-  `${GHPATH}/fonts/bootstrap-icons.woff2`,
-  `${GHPATH}/android-chrome-192x192.png`,
-  `${GHPATH}/android-chrome-512x512.png`,
-  `${GHPATH}/apple-touch-icon.png`,
-  `${GHPATH}/bootstrap-icons.css`,
-  `${GHPATH}/bootstrap.min.css`,
-  `${GHPATH}/bootstrap.min.js`,
-  `${GHPATH}/bootstrap.min.css.map`,
-  `${GHPATH}/bootstrap.min.js.map`,
-  `${GHPATH}/cookie.js`,
-  `${GHPATH}/favicon-16x16.png`,
-  `${GHPATH}/favicon-32x32.png`,
-  `${GHPATH}/favicon.ico`,
-  `${GHPATH}/jquery-3.6.4.min.js`,
-  `${GHPATH}/hrdict1.js`,
-  `${GHPATH}/hrdict2.js`,
-  `${GHPATH}/hrdict3.js`,
-  `${GHPATH}/endict.js`,
-  `${GHPATH}/main.css`,
-  `${GHPATH}/main.js`,
-  `${GHPATH}/manifest.json`,
-  `${GHPATH}/utils.js`
+var URLS = [
+    `${GHPATH}/`,
+    `${GHPATH}/fonts/bootstrap-icons.woff`,
+    `${GHPATH}/fonts/bootstrap-icons.woff2`,
+    `${GHPATH}/android-chrome-192x192.png`,
+    `${GHPATH}/android-chrome-512x512.png`,
+    `${GHPATH}/apple-touch-icon.png`,
+    `${GHPATH}/bootstrap-icons.css`,
+    `${GHPATH}/bootstrap.min.css`,
+    `${GHPATH}/bootstrap.min.js`,
+    `${GHPATH}/bootstrap.min.css.map`,
+    `${GHPATH}/bootstrap.min.js.map`,
+    `${GHPATH}/cookie.js`,
+    `${GHPATH}/favicon-16x16.png`,
+    `${GHPATH}/favicon-32x32.png`,
+    `${GHPATH}/favicon.ico`,
+    `${GHPATH}/jquery-3.6.4.min.js`,
+    `${GHPATH}/hrdict1.js`,
+    `${GHPATH}/hrdict2.js`,
+    `${GHPATH}/hrdict3.js`,
+    `${GHPATH}/endict.js`,
+    `${GHPATH}/main.css`,
+    `${GHPATH}/main.js`,
+    `${GHPATH}/manifest.json`,
+    `${GHPATH}/utils.js`
 ]
 
+self.addEventListener('fetch', function (event) {
+    let request = new Request(event.request.url, {cache: "reload"});
+    event.respondWith(
+        fetch(request).then(function (response) {
+            cache.put(request, response.clone());
+            return response;
+        }).catch(function () {
+            return caches.match(request);
+        }),
+    );
+});
+/*
 self.addEventListener('fetch', function (e) {
     console.log('Fetch request : ' + e.request.url);
     e.respondWith(
@@ -54,30 +66,30 @@ self.addEventListener('fetch', function (e) {
       })
     )
   })
-  
-  var CACHE_NAME = APP_PREFIX + VERSION;
-  self.addEventListener('install', function (e) {
+  */
+var CACHE_NAME = APP_PREFIX + VERSION;
+self.addEventListener('install', function (e) {
     e.waitUntil(
-      caches.open(CACHE_NAME).then(function (cache) {
-        console.log('Installing cache : ' + CACHE_NAME);
-        return cache.addAll(URLS)
-      })
-    )
-  })
-  
-  self.addEventListener('activate', function (e) {
-    e.waitUntil(
-      caches.keys().then(function (keyList) {
-        var cacheWhitelist = keyList.filter(function (key) {
-          return key.indexOf(APP_PREFIX)
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('Installing cache : ' + CACHE_NAME);
+            return cache.addAll(URLS)
         })
-        cacheWhitelist.push(CACHE_NAME);
-        return Promise.all(keyList.map(function (key, i) {
-          if (cacheWhitelist.indexOf(key) === -1) {
-            console.log('Deleting cache : ' + keyList[i] );
-            return caches.delete(keyList[i])
-          }
-        }))
-      })
     )
-  })
+})
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            var cacheWhitelist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX)
+            })
+            cacheWhitelist.push(CACHE_NAME);
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheWhitelist.indexOf(key) === -1) {
+                    console.log('Deleting cache : ' + keyList[i]);
+                    return caches.delete(keyList[i])
+                }
+            }))
+        })
+    )
+})
